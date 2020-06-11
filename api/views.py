a99cc6cbd902f1e4a4771ae18db93dc862b28580
florian <florian.even3@gmail.com>
2020-06-11 17:45:06 +0200

    add ratings option
    add number of rating in attribute and average of attribute

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import  viewsets,status
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from .models import  Movie,Rating
 from .serializers import MovieSerializer,RatingSerializer
@@ -10,12 +11,31 @@ from .serializers import MovieSerializer,RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
     @action(detail=True, methods=['POST'])
-    def rate_movie(self,request,pk=None):
+    def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
-            movie =Movie.objects.get(id=pk)
-            response = {'message' : 'its working'}
-            return Response(response, status=status.HTTP_200_OK)
+
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            #user = request.data
+            user = User.objects.get(id=1)
+            print('user',user.username)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'results':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating create', 'results': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+
         else:
             response = {'message' : 'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
