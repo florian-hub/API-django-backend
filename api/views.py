89930459bca36e6043c4e72cd2f82912bb903d8f
florian <florian.even3@gmail.com>
2020-06-11 17:58:51 +0200

    Token Authentification

--- api/views.py
+++ api/views.py
@@ -5,22 +5,21 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from .models import  Movie,Rating
 from .serializers import MovieSerializer,RatingSerializer
-
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-
+    authentication_classes = (TokenAuthentication,)
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
 
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            #user = request.data
-            user = User.objects.get(id=1)
-            print('user',user.username)
+            user = request.data
+        
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -43,3 +42,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
